09989e4744f702555e25408d88bb4ae0a6c4698f
Ben Brixey <ben1brixey@gmail.com>
2020-04-06 21:19:15 +0100

    animation update

--- main.js
+++ main.js
@@ -1,15 +1,63 @@
-var controller = new ScrollMagic.Controller();
-var nikeAnimation = gsap.timeline()
-    .from(".nike-title-1", {duration:2, x:-500, opacity: 0, ease: "bounce"})
-    .from(".nike-title-2", {y:200, opacity: 0})
-    .to(".nike-year", {text:"year || 2020", ease: "power1.in", duration:1}, "+=0.5")
-    .to(".nike-client", {text:"client || nike", ease: "power1.in", duration:1})
-    .to(".nike-agency", {text:"agency || Sept Studios", ease: "power1.in", duration:1});
+// Nike animation controls
 
-    var scene = new ScrollMagic.Scene({
-        triggerElement: "#nike-section",
-        triggerHook: 0.5,
-        reverse:false
-      })
-        .setTween(nikeAnimation)
-        .addTo(controller);
+var nikeController = new ScrollMagic.Controller();
+var nikeAnimation = gsap.timeline()
+  .to(".nike-year", {text:"year || 2020", ease: "power0", duration:1}, "+=0.5")
+  .to(".nike-client", {text:"client || nike", ease: "power0", duration:1})
+  .to(".nike-agency", {text:"agency || Sept Studios", ease: "power0", duration:2});
+
+var nikeScene = new ScrollMagic.Scene({
+  triggerElement: "#nike-section",
+  triggerHook: 0.5,
+  reverse:false
+})
+.setTween(nikeAnimation)
+  .addTo(nikeController);
+
+// Amex animation controls
+
+var amexController = new ScrollMagic.Controller();
+var amexAnimation = gsap.timeline()
+  .to(".amex-year", {text:"year || 2019", ease: "power0", duration:1}, "+=0.5")
+  .to(".amex-client", {text:"client || Amex", ease: "power0", duration:1})
+  .to(".amex-agency", {text:"music supervision || Native", ease: "power0", duration:2});
+
+var amexScene = new ScrollMagic.Scene({
+  triggerElement: "#amex-section",
+  triggerHook: 0.5,
+  reverse:false
+})
+.setTween(amexAnimation)
+  .addTo(amexController);
+
+// Swatch animation controls
+
+var swatchController = new ScrollMagic.Controller();
+var swatchAnimation = gsap.timeline()
+  .to(".swatch-year", {text:"year || 2020", ease: "power0", duration:1}, "+=0.5")
+  .to(".swatch-client", {text:"client || Swatch", ease: "power0", duration:1})
+  .to(".swatch-agency", {text:"agency || DBLG", ease: "power0", duration:1});
+
+var swatchScene = new ScrollMagic.Scene({
+  triggerElement: "#swatch-section",
+  triggerHook: 0.5,
+  reverse:false
+})
+.setTween(swatchAnimation)
+  .addTo(swatchController);
+
+// Swatch animation controls
+
+var ttpController = new ScrollMagic.Controller();
+var ttpAnimation = gsap.timeline()
+  .to(".ttp-year", {text:"year || 2019", ease: "power0", duration:1}, "+=0.5")
+  .to(".ttp-client", {text:"client || The Technology Project", ease: "power0", duration:2})
+  .to(".ttp-agency", {text:"agency || DBLG", ease: "power0", duration:1});
+
+var ttpScene = new ScrollMagic.Scene({
+  triggerElement: "#ttp-section",
+  triggerHook: 0.5,
+  reverse:false
+})
+.setTween(ttpAnimation)
+  .addTo(ttpController);
